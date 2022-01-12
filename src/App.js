4edac336e60d6761921423408950865eb1782d7c
osamahabib5 <osama.habib@khi.iba.edu.pk>
2022-01-12 22:20:25 +0500

    recreate project

--- src/App.js
+++ src/App.js
@@ -4,58 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div className="container">
-                <a className="navbar-brand" href="#"><img src="img/logo.png" alt=""/></a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#slider">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#about">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#services">Products</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact Us</a>
-                        </li>
-                      
-                        {/* <li className="nav-item dropdown">
-                            <a
-                                className="nav-link "
-                                href="#"
-                                id="navbarDropdown"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false">
-                                Blog
-                                <span className="fa fa-angle-down"></span>
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="../blog/index.html">Blog List</a>
-                                <a className="dropdown-item" href="../single-post/index.html">Single Post</a>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact</a>
-                        </li> */}
-                    </ul>
-                </div>
-            </div>
-        </nav> 
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
